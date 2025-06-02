efc32c906e887aa167c0c5fe56c0f53ebc5f43cc
Idkwest <nikachkhartishvili32@gmail.com>
2025-06-02 22:36:45 +0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,13 @@
-// Optional JS file (can be used for real-time stats later)
-console.log("Website loaded!");
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({
+    duration: 800,
+    easing: "ease-in-out",
+    once: true,
+  });
 
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = "opacity 1s ease";
+    document.body.style.opacity = 1;
+  }, 100);
+});
