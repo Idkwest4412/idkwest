17783c421287be8c5a43aa06f04b16db68c052e3
Idkwest <nikachkhartishvili32@gmail.com>
2025-06-02 22:42:22 +0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.style.opacity = 1;
   }, 100);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({ duration: 800, easing: "ease-in-out", once: true });
+
+  const sections = document.querySelectorAll("main > section, main > h2");
+  const navLinks = document.querySelectorAll("nav a");
+
+  function activateNavLink() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 150 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove("active"));
+    if(navLinks[index]) navLinks[index].classList.add("active");
+  }
+
+  activateNavLink();
+  window.addEventListener("scroll", activateNavLink);
+
+  // Smooth scrolling for nav links
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const href = link.getAttribute("href");
+      if (href.startsWith("#")) {
+        document.querySelector(href).scrollIntoView({ behavior: "smooth" });
+      } else {
+        window.location.href = href;
+      }
+    });
+  });
+});
